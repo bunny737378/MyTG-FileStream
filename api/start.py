2929a79aeb12f97a709f9787d49119f9673f5751
bunny737378 <bhupendrasahu69.com@gmail.com>
2025-07-22 17:50:32 +0530

    Update start.py

--- api/start.py
+++ api/start.py
@@ -1,5 +1,11 @@
 import os
+from http.server import BaseHTTPRequestHandler
 
-def handler(request, response):
-    os.system("python3 -m tgfs")
-    return response.send("tgfs module executed (once)")
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # This will run your tgfs bot
+        os.system("python3 -m tgfs")
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"tgfs bot triggered (may stop after timeout)")
+        return
